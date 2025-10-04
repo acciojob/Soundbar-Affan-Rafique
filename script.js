5bc16108d9ae2e129040cc2da3361f7f12f82e03
Affan-Rafique <affanrafique9@gmail.com>
2025-10-04 13:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+// Keep track of currently playing audio
+let currentAudio = null;
+
+// Get all buttons with class "btn"
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const soundFile = button.getAttribute("data-sound");
+    
+    // Stop previous audio if any
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    // Play new audio
+    currentAudio = new Audio(`sounds/${soundFile}`);
+    currentAudio.play();
+  });
+});
+
+// Stop button functionality
+const stopBtn = document.querySelector(".stop");
+stopBtn.addEventListener("click", () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentAudio = null;
+  }
+});
